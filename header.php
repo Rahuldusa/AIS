13163e63215ceef5f40ca53ff9995c4b7f8f5646
Rahulkumar <rahuldusa30@gmail.com>
2024-01-09 11:24:22 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,30 @@
+<header>
+    <div class="my-nav">
+        <div class="container">
+            <div class="row">
+                <div class="nav-items">
+                    <div class="menu-toggle">
+                        <div class="menu-hamburger"></div>
+                    </div>
+                    <div class="logo" style="width:25px;">
+                        <img src="images/AIS Logo Final.png" style="width:28%;height:100%;">
+                    </div>
+                    
+
+                    <div class="menu-items">
+                        <div class="menu">
+                            <ul>
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="about-us.php">About Us</a></li>
+                                <li><a href="services.php">Services</a></li>
+                                <li><a href="services.php">Gallery</a></li>
+                                <li><a href="contact-us.php">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</header>
---
+++ video.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/owl.carousel.css">
+    <link rel="stylesheet" href="assets/css/plugins/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+<?php include('header.php') ?>
+    <main>
+        <!-- ============abt-01 Section  Start============ -->
+    
+
+        <section class="abt-01">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="heading-wrapper">
+                            <h3>Video Gallery</h3>
+                            <ol>
+                                <li>Home<i class="far fa-angle-double-right"></i></li>
+                                <li>Video Gallery</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <!-- ============Footer  Start============ -->
+
+    <?php include('footer.php') ?>
+
+</body>
+
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/plugins/owl.carousel.js"></script>
+<script src="assets/js/plugins/owl.carousel.min.js"></script>
+<script src="assets/js/script.js"></script>
+
+</html>
